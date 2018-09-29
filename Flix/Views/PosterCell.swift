a86a608d3710f59e2ccd89f687530937abbba619
Ching Ching Huang <chingchinghuang@Chings-MacBook-Pro.local>
2018-09-29 02:00:52 -0500

    Add superheroViewController and collectionView

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Ching Ching Huang on 9/29/18.
+//  Copyright © 2018 Ching Ching Huang. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
